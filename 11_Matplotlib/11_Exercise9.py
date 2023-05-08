450298a547bd3e9b2c0ac4e033ba3aca99962330
Trần Đức Việt <132891334+Viet-DE@users.noreply.github.com>
2023-05-08 16:37:25 +0700

    Add files via upload

---
+++ 11_Matplotlib/11_Exercise9.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load data từ link Github
+url = 'https://github.com/thieu1995/csv-files/raw/main/data/matplotlib/company_sales_data.csv'
+df = pd.read_csv(url)
+
+# 
